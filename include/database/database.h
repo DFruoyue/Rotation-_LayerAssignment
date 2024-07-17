bd516f99b058f3d7d7a834ed245230b4cb5e111d
DFruoyue <df_ruoyue@stu.pku.edu.cn>
2024-07-17 10:35:11 +0800

    添加CMake

--- include/database/database.h
+++ include/database/database.h
@@ -2,16 +2,38 @@
 #include <vector>
 using namespace std;
 namespace XZA{
+    enum Direction{
+        HORIZONTAL = 0,
+        VERTICAL
+    };
+    class LayerInfo{
+        public:
+        string name;
+        Direction direction;
+        vector<vector<int>> capacity;
+    };
     class Database{
     private:
+        // Routing Resource
         int layerNum;
         int xSize;
         int ySize;
         double unitWLcost;
         int unitViacost;
-        vector<int> OFweight;
+        vector<int> OverFlowLayerWeight;
+        vector<int> HorizantalLengths;
+        vector<int> VerticalLengths;
+        vector<LayerInfo> layers;
+
+        // Net Information
+
+        // Load file
+        void load_Routing_Resource_file(const string& filename);
+        void load_Net_Information_file(const string& filename);
+
     public:
         Database();
         ~Database();
+        void load_data();
     };
 }
